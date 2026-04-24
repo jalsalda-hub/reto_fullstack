c172cab72f7299e92a04b69b8a0318f27a2b4ed4
JaimeLS <jalsalda@gmail.com>
2026-04-24 10:23:26 -0500

    feat: creala estructura atomic design y configura react-router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-gray-800">Bienvenidos a Hexashop</h1>
+      <p className="mt-4 text-gray-500">Atomic Design + Zustand + React Router</p>
+    </div>
+  );
+};
+
+export default Home;
