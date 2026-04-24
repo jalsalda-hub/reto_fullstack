33425fe16fcb8e3d50001637783af5cfdd31a65c
JaimeLS <jalsalda@gmail.com>
2026-04-24 11:21:21 -0500

    feat(Navbar): Enganchar funcionalidad de búsqueda en tiempo real de Zustand al SearchBar

--- src/components/molecules/SearchBar.jsx
+++ src/components/molecules/SearchBar.jsx
@@ -1,10 +1,22 @@
 import { Search } from 'lucide-react';
 import { Input } from '../atoms/Input';
+import { useProductStore } from '../../store/useProductStore';
 
 export const SearchBar = () => {
+  const { searchQuery, setSearchQuery } = useProductStore();
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <div className="relative w-full max-w-sm hidden md:flex items-center">
-      <Input placeholder="Buscar productos..." className="pr-10 rounded-full" />
+      <Input 
+        placeholder="Buscar productos..." 
+        className="pr-10 rounded-full" 
+        value={searchQuery}
+        onChange={handleSearch}
+      />
       <Search className="absolute right-3 text-hexa-gray w-5 h-5 cursor-pointer hover:text-hexa-dark" />
     </div>
   );
