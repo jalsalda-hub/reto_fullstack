780b7d2ab4c164411d25194b55f145bf09053256
JaimeLS <jalsalda@gmail.com>
2026-04-24 22:28:07 -0500

    Configuración para GitHub Pages

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import ProductDetail from '../pages/ProductDetail';
 import Login from '../pages/Login';
@@ -10,7 +10,7 @@ import { MainLayout } from '../components/templates/MainLayout';
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <MainLayout>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -22,7 +22,7 @@ const AppRouter = () => {
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </MainLayout>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
